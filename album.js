00054156c4f46bb93a7d85e75daa6fe728640d90
Hasib <49849757+hashh420@users.noreply.github.com>
2021-04-16 14:59:15 +0100

    added album.js

---
+++ album.js
@@ -0,0 +1,110 @@
+window.onload = function(){
+	albumDetails('75621062');
+}
+
+//window.addEventListener('DOMContentLoaded', function(){
+//	albumDetails('75621062');
+//})
+
+
+//	const combinedFunc = function(){
+//	//window.location.href = 'album.html';
+//	//lbumDetails('75621062');
+	
+//}
+
+
+//combinedFunc();
+let albums = [];
+
+//const getFetch = function(){
+	
+
+//		console.log(album.tracks.data);
+
+		const albumDetails = function(id){
+
+		fetch(`https://striveschool-api.herokuapp.com/api/deezer/album/${id}`)
+		.then(res => res.json())
+		.then(album =>{
+		const music = document.getElementById('daynamic');
+		music.innerHTML=
+			`<div class="col-12 col-lg-3 offset-lg-1 py-5">
+              <div class="d-flex justify-content-center">
+                <img
+                  src="${album.cover}"
+                  class="img-fluid shadow p-2 rounded"
+                />
+              </div>
+              <div class="album-label py-4">
+                <h2 id="album-title>${album.title}(Deluxe Edition 2011 Remaster)</h2>
+                <a
+                  class="artist d-flex justify-content-center text-white-50"
+                  href="artistPlusSidebar.html"
+                  >Queen</a
+                >
+
+                <div class="d-flex justify-content-center py-3">
+                  <button type="button" class="play-btn btn btn-success">
+                    PLAY
+                  </button>
+                </div>
+                <div class="d-flex justify-content-center">
+                  <span class="label year text-white-50 px-1">1995 </span>
+                  <span class="label text-white-50"
+                    ><i id="icons-small" class="bi bi-dot text-white-50"></i
+                  ></span>
+                  <span class="label text-nowrap text-white-50 px-1">
+                    15 SONGS
+                  </span>
+                </div>
+                <div class="d-flex justify-content-center py-4">
+                  <i id="icons-small" class="like liked bi bi-heart pr-3"></i>
+
+                  <i
+                    id="icons-small"
+                    class="threeDots bi bi-three-dots px-3 text-white-50"
+                  ></i>
+                </div>
+              </div>
+            </div>
+	`
+		album.tracks.data.forEach(element => {
+		music.innerHTML += `<div class="songs col-12 col-lg-8 py-2 py-lg-5">
+	              <div class="sgs row d-flex justify-content-center pb-3">
+	                <p class="text-white-50">
+	                  <i id="icons-small" class="bi bi-music-note"></i>
+	                </p>
+	                <div class="col-9">
+	                  ${element.title} <br /><a
+	                    href="artistPlusSidebar.html"
+	                    class="artist text-white-50"
+	                    ></a
+	                  >
+	                </div>
+	                <div class="col-2 text-white-50">1:13</div>
+	              </div>
+				  `;
+});
+
+	
+})
+}
+
+
+
+
+
+
+
+
+
+
+//const alb = document.querySelectorAll('#pageContent > div.albums.mt-5 > div > div')
+//for(let i =0; i < alb.length; i++){
+//alb[i].innerHTML = `
+//<divclass="single-album col-12 col-sm-4 col-lg-2 py-2 py-md-1 px-1 d-flex"
+//    style="justify-content: center; flex-direction: column">
+//              <img
+
+//}
